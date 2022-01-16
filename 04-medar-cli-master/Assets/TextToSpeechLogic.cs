29b7bb3acd4ffa6705b621b8bfff0b59d9b0f67a
Patlax <patricia.lakatosova@tum.de>
2022-01-16 17:51:17 +0100

    added WindowsVoice + SpokenText script

---
+++ 04-medar-cli-master/Assets/TextToSpeechLogic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class TextToSpeechLogic : MonoBehaviour, IInputClickHandler
+{
+    private TextToSpeech tts;
+    public string speakText;
+
+    private void Awake()
+    {
+        tts = GetComponent<TextToSpeech>();
+    }
+
+    public void OnInputClicked (InputClickedEventData eventData)
+    {
+        //create message
+        var message = string.Format(speakText, tts.Voice.ToString());
+
+        //spoken message
+        tts.StartSpeaking(message);
+    }
+   
+}
---
+++ 04-medar-cli-master/Assets/spokenText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using static WindowsVoice;
+
+public class spokenText : MonoBehaviour
+{
+    public string ouptut;
+    
+    private void Awake()
+    {
+       
+    }
+
+}
