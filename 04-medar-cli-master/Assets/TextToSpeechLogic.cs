63a01b20c8dbda51e45d4c0ac82140e7c2c1b033
Patlax <patricia.lakatosova@tum.de>
2022-01-21 14:18:44 +0100

    Animation should run during step 2 - pls check it :)

--- 04-medar-cli-master/Assets/TextToSpeechLogic.cs
+++ 04-medar-cli-master/Assets/TextToSpeechLogic.cs
@@ -22,30 +22,19 @@ public class TextToSpeechLogic : MonoBehaviour
     /// 
     public void GetText()
     {
-        // Find the Object with tag "syringe_" and get data from it
-        syringe = GameObject.FindGameObjectWithTag("syringe_");
-        //syringe_ toggle = syringe.GetComponent(typeof(toggle));
-
         //choosing the corresponding sentence
-        if (position == 0) { speakerstext = "Welcome to the training session. Once you've cleaned and numbed the site, you can pick up the needle."; }
+        if (position == 0) { speakerstext = "Welcome to the training session. Once you've cleaned and numbed the site, you can prepare the needle."; }
         else if (position == 1)
         {
-            speakerstext = "palpate the carotid artery with your left hand, covering the artery with your fingers. Insert the needle 0.5–1 cm laterally to the artery, aiming at a 45°angle to the vertical.";
+            speakerstext = "Palpate the carotid artery with your left hand, covering the artery with your fingers. Insert the needle 0.5 to 1 centimeter laterally to the artery, aiming at a 45 angle to the vertical.";
             //Start Animation
-            //toggle = syringe.GetComponent<toggle>();
-
+            syringe.GetComponent<toggle>().start_animation();
+            //alternatively
+            //public toggle Syringe = new toggle();
         }
-        else if (position == 2) { speakerstext = "Advance slowly, aspirating all the time, until you enter the vein.Enter 3 - 4 cm into the vein."; }
+        else if (position == 2) { speakerstext = "Advance slowly, aspirating all the time, until you enter the vein.Enter 3 to 4 centimeters into the vein."; }
         else if (position == 3) { speakerstext = "Remove the syringe, keeping the needle very still, and immediately put your thumb over the end of the needle. Great job, you sucessfully inserted the central line!"; }
-        else if (position > 3)
-        {
-            speakerstext = "Well done and see you soon";
-            //if animation is running, than stop it
-            if (position != 1)
-            {
-
-            }
-        }
+        else if (position > 3)  {FinishSpeaking();}
 
         Speak();
     }
@@ -72,10 +61,16 @@ public class TextToSpeechLogic : MonoBehaviour
     public void FinishSpeaking()
     {
         position = 4;
-        //stop animation
-
-        GetText();
+        textToSpeech.StartSpeaking("Well done and see you soon!");
+        //stop animation if it´s running
+        if (syringe.GetComponent<toggle>().isActiveAndEnabled) 
+        {
+            syringe.GetComponent<toggle>().stop_animation(); 
+        }
         //quit app
+        Application.Quit();
+        //to make sure it´s working
+        Debug.Log("Game is exiting");
     }
 
     public void Next()
