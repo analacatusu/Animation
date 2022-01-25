4a62356901bbba65306627544c8302268fca4b8b
andiz1014 <76585341+andiz1014@users.noreply.github.com>
2022-01-25 13:38:54 +0100

    add 3d model

---
+++ 04-medar-cli-master/Assets/Sponge.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sponge : MonoBehaviour
+{
+    public GameObject sponge;
+    private readonly float scale = 100F;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.CapsLock))
+        {
+            print("caps lock was pressed");
+
+            if (sponge.transform.localScale.x == 0 && sponge.transform.localScale.y == 0 && sponge.transform.localScale.z == 0)
+            {
+                StartAnimation();
+            }
+            else
+            {
+                StopAnimation();
+            }
+
+
+        }
+
+    }
+    public void StartAnimation()
+    {
+        sponge.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    public void StopAnimation()
+    {
+        sponge.transform.localScale = new Vector3(0, 0, 0);
+    }
+}
---
+++ 04-medar-cli-master/Assets/Syringe.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class Syringe : MonoBehaviour
+    {
+        public GameObject OBJECT1;
+        private readonly float scale = 0.05F;
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                print("space key was pressed");
+
+                if (OBJECT1.transform.localScale.x == 0 && OBJECT1.transform.localScale.y == 0 && OBJECT1.transform.localScale.z == 0)
+                {
+                    StartAnimation();
+                }
+                else
+                {
+                    StopAnimation();
+                }
+
+
+            }
+
+        }
+        public void StartAnimation()
+        {
+            OBJECT1.transform.localScale = new Vector3(scale, scale, scale);
+        }
+
+        public void StopAnimation()
+        {
+            OBJECT1.transform.localScale = new Vector3(0, 0, 0);
+        }
+    }
