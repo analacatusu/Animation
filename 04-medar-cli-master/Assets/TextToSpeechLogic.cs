426c196ec1217f74e647ba9b5aaf3c109eadf091
Patlax <patricia.lakatosova@tum.de>
2022-01-25 11:59:32 +0100

    Added new steps and changed the initialisation of 'textToSpeech' in TextToSpeechLogic

--- 04-medar-cli-master/Assets/TextToSpeechLogic.cs
+++ 04-medar-cli-master/Assets/TextToSpeechLogic.cs
@@ -5,86 +5,130 @@ using Microsoft.MixedReality.Toolkit.Audio;
 
 public class TextToSpeechLogic : MonoBehaviour
 {
-    //initialiting the position of the spoken text
+    ///initialiting the position of the spoken text
     int position = 0;
+    int negationCounter = 0;
     string speakerstext = "spaceholder";
 
-    //initializing TextToSpeech-script
-    readonly TextToSpeech textToSpeech = new TextToSpeech();
+    /// initializing TextToSpeech-script | new should not be used in combination with a Monobehaviour
+    public GameObject audioSource;
 
-
-    //include toggle into the script
+    /// Initialising the used objects
     public GameObject syringe;
+    public GameObject sponge;
+    public GameObject painkiller;
 
-    /// Sets the specified text dependent on the position
+    /// <summary>
+    /// Sets the specified text dependent on the position | should also be used for repeat [the current step] & continue, so the animations will be set awake again
+    /// <param name="speakerstext">actual output.</param>
     /// </summary>
-    /// <param name="speakerstext">The text to speak.</param>
-    /// 
+   
     public void GetText()
     {
-        //choosing the corresponding sentence
-        if (position == 0) { speakerstext = "Welcome to the training session. Once you've cleaned and numbed the site, you can prepare the needle."; }
-        else if (position == 1)
+        if (position < 8)
         {
-            speakerstext = "Palpate the carotid artery with your left hand, covering the artery with your fingers. Insert the needle 0.5 to 1 centimeter laterally to the artery, aiming at a 45 angle to the vertical.";
-            //Start Animation
-            syringe.GetComponent<Syringe>().StartAnimation();
-            //alternatively
-            //public toggle Syringe = new toggle();
-        }
-        else if (position == 2) 
-        { syringe.GetComponent<Syringe>().StopAnimation(); 
-          speakerstext = "Advance slowly, aspirating all the time, until you enter the vein.Enter 3 to 4 centimeters into the vein."; 
-        }
-        else if (position == 3) { speakerstext = "Remove the syringe, keeping the needle very still, and immediately put your thumb over the end of the needle. Great job, you sucessfully inserted the central line!"; }
-        else if (position > 3)  {FinishSpeaking();}
+            switch (position)
+            {
+                case 0: //cleaning and sterilising
+                    speakerstext = "Welcome to the training lesson for inserting a central line into the internal jungular vein. At first, drape the insertion site and clean it using the cotton pad and some antiseptic solution.";
+                    //show the animated sponge
+                    sponge.GetComponent<Sponge>().StartAnimation();
+                    break;
 
-        Speak();
+                case 1: //aplly local aneastetic
+                    sponge.GetComponent<Sponge>().StopAnimation();
+                    painkiller.GetComponent<PainKiller>().StartAnimation();
+                    speakerstext = "Next, draw up 10 ml of lidocaine. Infiltrate local anesthetic all around the site by touching the skin five times with the tip of your needle. Work down toward the vein.";
+                    break;
+
+                case 2: //determine vein and show US-image
+                    painkiller.GetComponent<PainKiller>().StopAnimation();
+                    speakerstext = "Let´s determine the location of the vein. - Take the transducer and place it tranversally just below the apex of the sternocleidomastoid muscle triangle. - The internal jungular vein will appear as a dark elliptical shape."; //[...with the orientation marker of the transducer directed to the patient´s left at 9 o´clock position]]
+                    //Show the ultrasound image
+
+                    break;
+
+                case 3: //Insertion
+                    speakerstext = "Now take the needle and insert it 0.5 to 1 centimeter laterally to the artery, aiming at a 45 angle to the vertical. In men, aim for the right nipple; in women, aim for the iliac crest.Advance slowly, aspirating all the time, until you enter 3 to 4 centimeter into the vein. - Are you able to aspirate blood?";
+                    syringe.GetComponent<Syringe>().StartAnimation();
+                    break;
+
+                case 4: //Negation - blood was not aspirated
+                    syringe.GetComponent<Syringe>().StopAnimation();
+                    speakerstext = "Withdraw the needle, re-enter at the same point, but aim slightly more medially. - Are you now able to aspirate blood?";
+                    syringe.GetComponent<Syringe>().StartAnimation();
+                    break;
+
+                case 5: //Blood was aspirated > remove the syringe
+                    negationCounter = 0;
+                    syringe.GetComponent<Syringe>().StopAnimation();
+                    speakerstext = "Great! Finally,Remove the syringe. "; //...keeping the needle very still, and immediately put your thumb over the end of the needle.
+                    break;
+                    /*
+                case 6: //Here, The wire should be inserted
+                    speakerstext = "";
+                    break;
+                    */
+                case 6: //clear after insertion
+                    speakerstext = "Clean the skin around the line using the cotton pad once more, dry, and cover it with occlusive dressings.";
+                    sponge.GetComponent<Sponge>().StartAnimation();
+                    break;
+
+                case 7: FinishSpeaking();
+                    break;
+            }
+        } else FinishSpeaking();
+
+        GetTextToSpeechStartSpeaking();
     }
 
     /// <summary>
     /// calls the StartSpeaking function from TextToSpeech, which synthetizes the audio > also used for continue and repeat
     /// </summary>
-    /// 
-    public void Speak()
+    public void GetTextToSpeechStartSpeaking() 
     {
+//initializing the TextToSpeech-Schript locally
+        TextToSpeech textToSpeech = audioSource.GetComponent<TextToSpeech>();
         textToSpeech.StartSpeaking(speakerstext);
     }
 
-    /// <summary>
-    /// functions corresponding to the input > manage the position trough changing it
-    /// </summary>
-    /// 
-    public void StartSpeaking()     //for "start" and "restart training"
+    public void StartSpeaking()     //applicable for "start" and "restart training"
     {
         position = 0;
+        negationCounter = 0;
         GetText();
     }
 
     public void FinishSpeaking()
     {
-        position = 4;
-        textToSpeech.StartSpeaking("Well done and see you soon!");
-        //stop animation if it´s running
-        if (syringe.GetComponent<Syringe>().isActiveAndEnabled) 
+//stop all animations
+        sponge.GetComponent<Sponge>().StopAnimation();
+        syringe.GetComponent<Syringe>().StopAnimation();
+        painkiller.GetComponent<PainKiller>().StopAnimation();
+
+//initializing the TextToSpeech-Schript locally
+        TextToSpeech textToSpeech = audioSource.GetComponent<TextToSpeech>();
+//if the user made too many mistakes
+        if (negationCounter >1 && (position == 3 || position == 4))
         {
-            syringe.GetComponent<Syringe>().StopAnimation(); 
+             textToSpeech.StartSpeaking("Insertion failed. Please restart or quit the session.");        //GetComponent(TextToSpeech).StartSpeaking("Insertion failed. Please restart or quit the session.");
         }
-        //quit app
-        Application.Quit();
-        //to make sure it´s working
-        Debug.Log("Game is exiting");
+//if user succeeded
+        else textToSpeech.StartSpeaking("Well done, you succeeded!");
+//to make sure that the app will quit, the position is being changed to a number > 8
+        position = 7;
     }
 
     public void Next()
     {
-        //check if it´s the last step > if not, go to the next step
-        if (position <= 4)
+//at position 3, skip the "failed insertion"-step
+        if(position == 3)
         {
-            position++;
-            GetText();
-        }
-        else FinishSpeaking();
+            position = 5;
+        } 
+        else if(position <=7) position++;
+
+        GetText();
     }
 
     public void Back()
@@ -94,15 +138,60 @@ public class TextToSpeechLogic : MonoBehaviour
         {
             position--;
         }
+        //check if it´s the "insertion-succeeded"-step. if it is this one, do not go to the "insertion failed"-step but to the previous one
+        if (position <= 4 && position < 7)
+        {
+            negationCounter = 0;
+            position=3;
+        }
         GetText();
     }
 
-    /// <summary>
-    /// Stops speaking
-    /// </summary>
-    /// 
     public void StopSpeaking()
     {
+//initializing the TextToSpeech-Schript locally
+        TextToSpeech textToSpeech = audioSource.GetComponent<TextToSpeech>();
         textToSpeech.StopSpeaking();
+//stop all running animations
+        sponge.GetComponent<Sponge>().StopAnimation();
+        syringe.GetComponent<Syringe>().StopAnimation();
+        painkiller.GetComponent<PainKiller>().StopAnimation();
+    }
+
+    public void AspirationFailed() //for the "no"-answer
+    {
+        negationCounter++;
+        //aspiration failed for the first time
+        if (position == 3 && negationCounter < 2)
+        {
+            position++;
+            GetText();
+        }
+        //For the 2nd try, repeat the question - the animation is already running
+        else if (position == 4 && negationCounter < 2)
+        {
+            GetTextToSpeechStartSpeaking();
+        }
+        //if the blood aspiration or the needle insertion fails two times, finish the training
+        else if ((position == 3 || position == 4) && negationCounter >= 2)
+        {
+            FinishSpeaking();
+        }
+        //If "no" is not an appropriate answer at this moment, repeat the cuurent step until something else is being said
+        else GetText();
+    }
+
+    public void Approved()
+    {
+        if(position == 3)
+        { 
+            position=5; 
+        }
+        else if(position == 4)
+        {
+            position++;
+        }
+//If "yes" is not an appropriate answer at this moment, repeat the current step until something else is being said
+        GetText();
     }
 }
